984612f8823a73a0b7d9fd7f964e0156e7d1a0f0
Raynos <raynos2@gmail.com>
2012-11-16 21:09:26 -0800

    api?

--- test/summing.js
+++ test/summing.js
@@ -1,10 +1,14 @@
 var test = require("tap").test
     , assert = require("assert")
+    , toArray = require("write-stream").toArray
+
     , MapReduce = require("..")
 
 test("sum", function (t) {
-    var db = MapReduce("./fixtures")
+    // Open a map reduce db on the levelup uri
+    var db = MapReduce("/tmp/sum")
 
+    // Define a map reduce query
     db.view("sum", function map(number, emit) {
         emit("id", number)
     }, function reduce(key, values) {
@@ -13,5 +17,18 @@ test("sum", function (t) {
         }, 0)
     })
 
-    var stream = db.query("sum")
+    // Query the db to run the map reduce on a range of keys
+    var stream = db.query("sum", {
+        start: "0"
+        , end: "9"
+    })
+
+    // wait for data to be reduced
+    stream.pipe(toArray(function (list) {
+        var value = list[0]
+
+        t.equal(value[0], "id")
+        t.equal(value[1], (1000 * 1001) / 2)
+        t.end()
+    }))
 })
