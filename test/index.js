8d46b6625d5a96a75f7a89048356194c420cb9d5
Raynos <raynos2@gmail.com>
2012-11-16 21:00:10 -0800

    removal

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var test = require("tap").test
-    , assert = require("assert")
-    , MapReduce = require("..")
-
-test("sum", function (t) {
-    var db = MapReduce("./fixtures")
-
-    db.view("")
-})
