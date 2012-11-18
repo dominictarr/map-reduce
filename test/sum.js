1300978f587bcd2c0dd77f389d5f681b6420c984
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-17 21:13:16 -0800

    split tests

--- test/sum.js
+++ test/sum.js
@@ -7,6 +7,7 @@ var through = require('through')
 var mac     = require('macgyver')().autoValidate()
 var pad     = require('pad')
 
+
 sum('/tmp/map-reduce-sum-test', function (err) {
   if (err) {
       throw err
@@ -36,28 +37,3 @@ sum('/tmp/map-reduce-sum-test', function (err) {
   }).times(3))
 })
 
-sum('/tmp/map-reduce-sum-test-range', function (err) {
-  if (err)
-    throw err
-
-  var mr = MR({
-      path: '/tmp/map-reduce-sum-test-range'
-    , start: '00000'
-    , end: '00002'
-    , map: function (key, value) {
-      console.log("v", value)
-      this.emit("const", value)
-    }
-    , reduce: function (big, little, key) {
-        return JSON.stringify(JSON.parse(big) + JSON.parse(little))
-      }
-    , initial: 0
-  }).force()
-
-  mr.on('reduce', function (key, sum) {
-    console.log('range', sum)
-    // 0, 1, 2 and 10000 are the individuals. Then 10-19,
-    // 100-199 and 1000-1999
-    assert.equal(JSON.parse(sum), (19 * 20) / 2)
-  })
-})
