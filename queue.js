31258c4346e75e83c4482fcd1a212678f06e4ebd
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-19 19:22:19 -0800

    refactor queue

--- queue.js
+++ queue.js
@@ -4,11 +4,19 @@ LevelUp Queue.
 
 var through = require('through')
 
-module.exports = function (db, prefix, work, ready) {
+module.exports = function (prefix, work) {
+
+return function (db) {
+
   var jobs = [], active = {}, I = 0, batch = []
 
-  if(work)
-    db.on('queue:start', work)
+  if('string' !== typeof prefix)
+    prefix = '~QUEUE', work = prefix
+
+  if(work && 'object' === typeof work) {
+    for(var job in work)
+      db.on('queue:start:'+job, work)
+  }
 
   /**
   on start up, read any unfinished jobs from database 
@@ -18,32 +26,55 @@ module.exports = function (db, prefix, work, ready) {
   (possible if a delete fails and that job is requeued concurrently)
 
   then start the jobs.
+
+  make i into a timestamp! a monotonic-timestamp
   **/
 
+  var inProgress = 0
+  var count = 0
+  
   db.readStream({start: prefix , end: prefix+'~~'})
-    .pipe(through(function (data) {
-      var key = ''+data.value
-      var i   = (''+data.key).split('~').pop()
-      db.emit('queue:recover', key)
-      start(key, i)
-      I = Math.max(I, i)
-    }, function () {
-      db.queue = queue
-      db.emit('queue:ready', queue)
-      if(ready) ready(queue)      
-    }))
+    .on('data', function (data) {
+      count ++
+      //KEY should be VALUE
+      var value = ''+data.value
+      var ary = (''+data.key).split('~')
+      var i   = ary.pop()
+      var job = ary.pop()
 
-  function toKey(key, i) {
-    var k = [prefix, key, i.toString()].join('~')
+      db.emit('queue:recover', job, value)
+      db.emit('queue:recover:'+job, value)
+      start(job, i, value)
+      I = Math.max(I, i)
+    })
+    .on('end', function () {
+      //emit drain if there was no data.
+      if(!count)
+        db.emit('queue:drain')
+    })
+
+  function toKey(job, i) {
+    var k = [prefix, job, i.toString()].join('~')
     return k
   }
 
-  function start (key, i) {
-    db.emit('queue:start', key, function () {
-      db.del(toKey(key, i), function () {
-        db.emit('queue:done', key)
+  function start (job, i, value) {
+    inProgress ++
+    function done () {
+      db.del(toKey(job, i), function () {
+        inProgress --
+        try {
+          db.emit('queue:done', job, value)
+          db.emit('queue:done:'+job, value)
+        } finally {
+          if(!inProgress)
+            db.emit('queue:drain', key)
+        }
       })
-    })
+    }
+    db.emit('queue:start', job, value, done)
+    //you should probably just use this pattern...
+    db.emit('queue:start:'+job, value, done)
   }
 
   /**
@@ -53,24 +84,28 @@ module.exports = function (db, prefix, work, ready) {
     (even if the process crashes immediately after)
   **/
 
-  function queue (key, batch) {
-    var keys = Array.isArray(key) ? key : [key] 
+  function queue (job, value, batch) {
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    //CHANGING KEY -> VALUE
+    var values = Array.isArray(value) ? value : [value] 
     batch = batch || []
     var toStart = []
 
-    keys.forEach(function (key) { 
-      toStart.push({key: key, i: ++I})
-      batch.push({type: 'put', key: toKey(key, I), value: key})
+    values.forEach(function (value) { 
+      toStart.push({key: job, i: ++I, value: value})
+      batch.push({type: 'put', key: toKey(job, I), value: value})
     })
 
     if(batch.length)
       db.batch(batch, function (err) {
         if(err) throw err //? is this the right thing to do here?
         toStart.forEach(function (a) {
-          start(a.key, a.i)
+          start(a.key, a.i, a.value)
         })
       })
   }
 
-  return db
+  db.queue = queue
+
+}
 }
