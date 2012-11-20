fe09e4e7fcd433c93f0463380a32cc938333ba21
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-19 19:22:41 -0800

    tidy

--- test/fixtures/sum.js
+++ test/fixtures/sum.js
@@ -6,12 +6,18 @@ var pad     = require('pad')
 function genSum (path, cb) {
   rimraf(path, function () {
     levelup(path, {createIfMissing: true}, function (err, db) {
+
+      //install plugin system
+      require('../../use')(db)
+
       var l = 1e3, i = 0
       var stream = db.writeStream()
       while(l--)
         stream.write({key: pad(6, ''+ ++i, '0'), value: JSON.stringify(i)})
       stream.end()
-      if(cb) stream.on('close', cb)
+      if(cb) stream.on('close', function () {
+        cb(null, db)
+      })
     })
   })
 }
@@ -19,7 +25,6 @@ function genSum (path, cb) {
 if(!module.parent) {
 
   var dir = '/tmp/map-reduce-sum-test'
-
   genSum()
 }
 
