0fa3ca4156e242ba6cef1e3dff9c5120df4a92ad
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-20 01:43:05 -0800

    indent

--- queue.js
+++ queue.js
@@ -2,110 +2,111 @@
 LevelUp Queue.
 **/
 
-var through = require('through')
+var through   = require('through')
+var timestamp = require('monotonic-timestamp')
 
 module.exports = function (prefix, work) {
 
-return function (db) {
+  return function (db) {
 
-  var jobs = [], active = {}, I = 0, batch = []
+    var jobs = [], active = {}, batch = []
 
-  if('string' !== typeof prefix)
-    prefix = '~QUEUE', work = prefix
+    if('string' !== typeof prefix)
+      work = prefix, prefix = '~queue'
 
-  if(work && 'object' === typeof work) {
-    for(var job in work)
-      db.on('queue:start:'+job, work)
-  }
-
-  /**
-  on start up, read any unfinished jobs from database 
-  (incase there was a crash)
-
-  cleanup any duplicates, 
-  (possible if a delete fails and that job is requeued concurrently)
-
-  then start the jobs.
-
-  make i into a timestamp! a monotonic-timestamp
-  **/
-
-  var inProgress = 0
-  var count = 0
-  
-  db.readStream({start: prefix , end: prefix+'~~'})
-    .on('data', function (data) {
-      count ++
-      //KEY should be VALUE
-      var value = ''+data.value
-      var ary = (''+data.key).split('~')
-      var i   = ary.pop()
-      var job = ary.pop()
-
-      db.emit('queue:recover', job, value)
-      db.emit('queue:recover:'+job, value)
-      start(job, i, value)
-      I = Math.max(I, i)
-    })
-    .on('end', function () {
-      //emit drain if there was no data.
-      if(!count)
-        db.emit('queue:drain')
-    })
-
-  function toKey(job, i) {
-    var k = [prefix, job, i.toString()].join('~')
-    return k
-  }
-
-  function start (job, i, value) {
-    inProgress ++
-    function done () {
-      db.del(toKey(job, i), function () {
-        inProgress --
-        try {
-          db.emit('queue:done', job, value)
-          db.emit('queue:done:'+job, value)
-        } finally {
-          if(!inProgress)
-            db.emit('queue:drain', key)
-        }
-      })
+    if(work && 'object' === typeof work) {
+      for(var job in work)
+        db.on('queue:start:'+job, work[job])
     }
-    db.emit('queue:start', job, value, done)
-    //you should probably just use this pattern...
-    db.emit('queue:start:'+job, value, done)
-  }
 
-  /**
-    pass array of keys + batch to associate jobs with a batch insert.
-    this will mean that if the PUT can't be done without scheduling the jobs.
+    /**
+    on start up, read any unfinished jobs from database 
+    (incase there was a crash)
 
-    (even if the process crashes immediately after)
-  **/
+    cleanup any duplicates, 
+    (possible if a delete fails and that job is requeued concurrently)
 
-  function queue (job, value, batch) {
-    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    //CHANGING KEY -> VALUE
-    var values = Array.isArray(value) ? value : [value] 
-    batch = batch || []
-    var toStart = []
+    then start the jobs.
 
-    values.forEach(function (value) { 
-      toStart.push({key: job, i: ++I, value: value})
-      batch.push({type: 'put', key: toKey(job, I), value: value})
-    })
+    make i into a timestamp! a monotonic-timestamp
+    **/
 
-    if(batch.length)
-      db.batch(batch, function (err) {
-        if(err) throw err //? is this the right thing to do here?
-        toStart.forEach(function (a) {
-          start(a.key, a.i, a.value)
-        })
+    var inProgress = 0
+    var count = 0
+
+    function emit(name, a, b) {
+      if(!a)
+        return db.emit('queue:' + name)
+      db.emit('queue:'+name,    a, b)
+      db.emit('queue:'+name+':'+a, b)
+    }
+  
+    db.readStream({start: prefix , end: prefix+'~~'})
+      .on('data', function (data) {
+        count ++
+        //KEY should be VALUE
+        var value = ''+data.value
+        var ary = (''+data.key).split('~')
+        var t   = ary.pop()
+        var job = ary.pop()
+
+        emit('recover', job, value)
+        start(job, t, value)
       })
+      .on('end', function () {
+        //emit drain if there was no data.
+        if(!count)
+          db.emit('queue:drain')
+      })
+
+    function toKey(job, ts) {
+      return [prefix, job, (ts || timestamp()).toString()].join('~')
+    }
+
+    function start (job, ts, value) {
+      inProgress ++
+      function done () {
+        db.del(toKey(job, ts), function () {
+          inProgress --
+          try {
+            db.emit('queue:done', job, value)
+            db.emit('queue:done:'+job, value)
+          } finally {
+            if(!inProgress)
+              db.emit('queue:drain')
+          }
+        })
+      }
+      db.emit('queue:start', job, value, done)
+      //you should probably just use this pattern...
+      db.emit('queue:start:'+job, value, done)
+    }
+
+    /**
+      pass array of keys + batch to associate jobs with a batch insert.
+      this will mean that if the PUT can't be done without scheduling the jobs.
+
+      (even if the process crashes immediately after)
+    **/
+
+    function queue (job, value, batch) {
+      batch = batch || []
+      var key = toKey(job, ts)
+
+      var ts = timestamp()
+        batch.push({
+          type: 'put', 
+          key: key, 
+          value: value
+        })
+
+      if(batch.length)
+        db.batch(batch, function (err) {
+          if(err) throw err //? is this the right thing to do here?
+          start(job, ts, value)
+        })
+    }
+
+    db.queue = queue
   }
-
-  db.queue = queue
-
-}
 }
