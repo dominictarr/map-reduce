d8539cd582b72b90fd5a0d1acb217d93e76d8741
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-20 17:36:01 -0800

    use range-buckets to make groups

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var EventEmitter = require('events').EventEmitter
 var through = require('through')
 var levelup = require('levelup')
 var queuer  = require('./queue')
+var Bucket  = require('./range-bucket')
 
 function sk (ary) {
   if(!Array.isArray(ary))
@@ -18,6 +19,13 @@ function bufferToString(b) {
 
 module.exports = function (opts) {
 
+  var bucket = Bucket('mapr')
+
+  var range = {
+    start: (opts.range ? opts.range.start : opts.start) || '',
+    end  : (opts.range ? opts.range.end   : opts.end  ) || '~'
+  }
+
   return function (db) {
 
   var emitter = db
@@ -26,7 +34,6 @@ module.exports = function (opts) {
   
   db.use(queuer({
     map: function (job, done) {
-      job = JSON.parse(job)
       db.get(job, function (err, doc) {
         doMap({key: job, value: doc}, done)
       })
@@ -53,8 +60,10 @@ module.exports = function (opts) {
   var queue = db.queue.bind(db)
 
   db.on('put', function (key, value) {
-    if(key < '~')
-      queue('map', bufferToString(key))
+    //all docs
+    key = key.toString()
+    if(range.start <= key && key <= range.end)
+      queue('map', key)
   })
   db.on('del', function (key) {
     //NOT IMPLEMENTED YET!
@@ -63,38 +72,37 @@ module.exports = function (opts) {
   var initial = opts.initial
   var reducers = {}, rTimeout
 
-  function group(key) {
-    var start = key.slice()
-    var end   = key.slice()
-    start.push('')
-    end.push('~')
-    return {start: sk(start), end: sk(end)}
-  }
-
   function doReduce (key, cb) {
     if(!Array.isArray(key))
       key = JSON.parse(key)
-    var collection = initial
 
+    var collection = initial
     var values = []
 
-    db.readStream(group(key))
+    key.push(true)
+
+    db.readStream(bucket.range(key))
       .pipe(through(function (data) {
         collection = reduce(collection, data.value, data.key)
       }, function () {
         //save the collection
-        db.emit('reduce', key.slice(1),collection)
 
-        key[0] = key[0] - 1
+        //get the parent group
+        var _key = key.slice(); _key.pop()
+
+        db.emit('reduce', _key, collection)
         //TODO: when queuing, write a queue message to the DB.
         //do it in a batch with the main update.
         //leveldb is optomized for batch updates, so this will be fast.
-        db.put(sk(key), collection, function (err) {
-          if(key[0] <= 0) return
+        db.put(bucket(_key), collection, function (err) {
+          if(!_key.length) return
+          //if(key[0] <= 0) return
 
-          //queue the larger group to be reduced.
-          key.pop()
-          queue('reduce', bufferToString(key))
+          //queue the parent group to be reduced.
+          _key.pop()
+          //if this job has been queued but not started can we
+          //avoid this write?
+          queue('reduce', JSON.stringify(_key))
           cb(err)
         })
       }))
@@ -102,32 +110,21 @@ module.exports = function (opts) {
 
   function doMap (data, cb) {
     var keys = [], sync = true, self = this
-    //change the string key into a group key.
 
-    function queueK (key, id) {
-      if(!Array.isArray(key)) key = [key]
-      key.unshift(key.length + 1)
-
-      queue('reduce', bufferToString(key.slice()))
-
-      key.push(id.toString())
-      return sk(key)
-    }
-    //store the doc key -> mapped keys,
-    //so that if the doc is updated,
-    //and emits different keys
-    //then we can remove the old maps.
-    db.get('~MAPKEYS~'+data.key, function (err, oldKeys) {
+    db.get(bucket('map', /*map,*/ data.key), function (err, oldKeys) {
       oldKeys = oldKeys ? JSON.parse(oldKeys) : []
       var maps = []
       function emit (key, value) {
         if(!sync) throw new Error('emit called asynchronously')
         if(!~keys.indexOf(key)) {
           keys.push(key)
+          //queue a new reduce job.
+          queue('reduce', JSON.stringify([key])) //bufferToString(key.slice()))
+          console.log('SAVE', bucket([key, data.key]).toString())
           maps.push({
             type: 'put',
             //also, queue the next reduce.
-            key: queueK(key, data.key),
+            key: bucket([key, data.key]),
             value: value
           })
         }
@@ -145,45 +142,15 @@ module.exports = function (opts) {
     })
   }
 
+  //force the map-reduce to run.
   db.startMapReduce = function (key) {
-  
-    //force the map-reduce to run.
-    db.readStream(opts)
+
+    //delete range first?
+    db.readStream(range)
       .pipe(through(doMap))  
 
     return db
   }
-
-  db.viewStream = function (opts) {
-    //get a stream from a map-reduce...
-  }
-
-  //read the results of a map-reduce
-/*
-  emitter.readStream = function (opts) {
-    //if opts.group is an array, use it to set start, end
-
-    //abstract this out
-
-    if(Array.isArray(opts.group)) {
-      opts.group.unshift(opts.group.length + 1)
-      var _opts = group(opts.group)
-      opts.start = _opts.start
-      opts.end = _opts.end
-    } else {
-      opts.start = ''; opts.end = '~'
-    }
-
-    //abstract this out, and provide access to the other levelup streams.
-    if(db)
-      return db.readStream(opts)
-    var t = through ()
-    emitter.once('load', function (db) {
-      db.readStream(opts).pipe(t)
-    })
-    return t
-  }
-*/
 
   }
 }
