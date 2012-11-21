9f877c7aa3002b413ec0c58b3c153a86f9086857
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-20 19:33:23 -0800

    use post hook in queue

--- queue.js
+++ queue.js
@@ -4,6 +4,7 @@ LevelUp Queue.
 
 var through   = require('through')
 var timestamp = require('monotonic-timestamp')
+var posthook  = require('./posthook')
 
 module.exports = function (prefix, work) {
 
@@ -40,24 +41,36 @@ module.exports = function (prefix, work) {
       db.emit('queue:'+name,    a, b)
       db.emit('queue:'+name+':'+a, b)
     }
-  
+
+    function onJob (data) {
+      //KEY should be VALUE
+      var value = ''+data.value
+      var ary = (''+data.key).split('~')
+      var t   = ary.pop()
+      var job = ary.pop()
+      emit('recover', job, value)
+      start(job, t, value)
+    }
+
+    //read any jobs left from last run.
     db.readStream({start: prefix , end: prefix+'~~'})
       .on('data', function (data) {
-        count ++
-        //KEY should be VALUE
-        var value = ''+data.value
-        var ary = (''+data.key).split('~')
-        var t   = ary.pop()
-        var job = ary.pop()
-
-        emit('recover', job, value)
-        start(job, t, value)
+        count=true; onJob(data)
       })
       .on('end', function () {
         //emit drain if there was no data.
-        if(!count)
+        if(!count && !inProgress)
           db.emit('queue:drain')
       })
+
+    //listen for new jobs.
+    db.use(posthook(function (change) {
+      if(change.type == 'put' && /^~queue/.test(change.key)) {
+        console.log('POSTHOOK', change.key+'', change.value+'')
+        onJob(change)
+      }
+    }))
+
 
     function toKey(job, ts) {
       return [prefix, job, (ts || timestamp()).toString()].join('~')
@@ -89,22 +102,20 @@ module.exports = function (prefix, work) {
       (even if the process crashes immediately after)
     **/
 
-    function queue (job, value, batch) {
-      batch = batch || []
+    function queue (job, value, put) {
+      var ts = timestamp()
       var key = toKey(job, ts)
 
-      var ts = timestamp()
-        batch.push({
+/*      if(put === false) {
+        //return the job to be queued, to include it in a batch insert.
+          return {
           type: 'put', 
           key: key, 
           value: value
-        })
-
-      if(batch.length)
-        db.batch(batch, function (err) {
-          if(err) throw err //? is this the right thing to do here?
-          start(job, ts, value)
-        })
+        }
+      } else {*/
+        db.put(key, value)
+      //}
     }
 
     db.queue = queue
