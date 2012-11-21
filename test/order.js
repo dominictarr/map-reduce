0f71acd6f0f4fbfee32a9c2955c5c59ee9258ae2
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-20 17:38:07 -0800

    rm stuff that doesn't matter

--- test/order.js
+++ test/order.js
@@ -1,10 +1,22 @@
 
-
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
+var assert = require('assert')
+var alphabet = '$&[{}(=*)+]!#%7531902468`"\'?^/@\|-_abcdefghijklmnopqrstuvwxyz,.:;<>\\~'
+console.log(alphabet.split('').sort().join(''))
 function randomLetter(n) {
-  var a = alphabet[~~(Math.random()*26)]  
-  return n ? a + randomLetter(n - 1) : a
+  var a = alphabet[~~(Math.random()*26)]
+  return (n ? a + randomLetter(n - 1) : a).toUpperCase()
+}
+
+var sep = ','
+
+function toKey(g) {
+  return g.map(function (e) {
+    return encodeURIComponent(e)
+  }).join(sep)
+}
+
+function fromKey (a) {
+  return a.split(sep).map(decodeURIComponent)
 }
 
 var groups = []
@@ -15,14 +27,13 @@ function gen (a) {
   if(a.length > 3) return
 
   while(l --) {
-    var _a = JSON.parse(JSON.stringify(a))
+    var _a = a.slice()
     _a.push(randomLetter(3))
     gen(_a)
     _a.unshift(_a.length)
-    groups.push(JSON.stringify(_a))
+    groups.push(toKey(_a))
   }
 }
 
 gen()
 
-console.log(groups.sort())
