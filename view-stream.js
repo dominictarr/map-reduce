4ee17a844d0ce743194c2453ebb21b3e56081bdd
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-02 23:36:45 -0800

    tidy

--- view-stream.js
+++ view-stream.js
@@ -8,13 +8,13 @@ module.exports = function (db, viewer) {
     opts = 'string' === typeof opts ? {name: opts} : opts
     var view = viewer.views[opts.name]
     var range = view.bucket.range(opts.start, opts.end)
+    console.log(range)
     opts.start = range.start; opts.end = range.end
 
     var ls = opts.tail === false ? db.readStream(opts) : db.liveStream(opts)
 
     return ls.pipe(through(function (data) {
         var _data = {key: view.bucket.parse(data.key).key, value: data.value}
-        console.log('view', _data)
         this.queue(_data)
       })).once('close', ls.destroy.bind(ls))
   }
